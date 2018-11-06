f9f34444d35d1887949feb25f15329403dcc15eb
ivan4000 <sxln1@mail.ru>
2018-11-06 16:04:39 +0300

    AWTtemplate

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,25 @@
 package com;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame jFrame = getFrame();
+        jFrame.add(new MyComponent());
+    }
+    static class MyComponent extends JComponent {
+        @Override
+        public void paint(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+        }
+    }
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {};
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 500);
+        return jFrame;
     }
 }
